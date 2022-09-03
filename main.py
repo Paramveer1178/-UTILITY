46daf277a3ce40a649f4352b6341f387afa53f8f
Paramveer1178 <92803884+Paramveer1178@users.noreply.github.com>
2022-09-03 22:41:47 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+import discord
+
+client = discord.event()
+ async def on_ready();
+    print("YOUR BOT IS READY") 
+
+
+
+client.run("MTAxNTY1ODM3NDc1NjU4NTUwMg.GpPPss.UEhGf7UsAYjqvy-YOdJNpRdcmcHQapHHmBBwj4") 
